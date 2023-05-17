5fc66882b42944d77f37115a313ba6a98b7c0244
ashna-24 <93767921+ashna-24@users.noreply.github.com>
2023-05-17 18:40:07 +0530

    commit

--- Exercise 3/js/script.js
+++ Exercise 3/js/script.js
@@ -2,16 +2,32 @@ var data = [
     {"firstname":" Ann", "lastname":" Wilson ", "phone": "29378245", "zip": "2 0 00 1 ", "age": 160, "id": " 12345 "},
     {"firstname":"Chris ", "lastname":"Morgan", "phone": "729378245", "zip": " 65 00 1", "age": 70, "id": " 123453543636 "},
     {"firstname":" Andrew ", "lastname":" Storm", "phone": "543729378245", "zip": " 7001 4", "age": 32, "id": "5 648"}
-]
+];
 
-data.forEach( objarray=> {
-    for(const value in objarray){
-        let result = objarray[value];
-        let resarray = new Array(result).toString();
-        console.log(resarray); 
+for(let i in data){
+    let result = data[i];
+    
+    result["Name:"]=(result["firstname"]+""+result["lastname"]).trim();
+    console.log("Name:",result["Name:"]);
+
+    if(result["phone"].length == 10){
+        console.log("Phone:",result["phone"]);
+    }
+    else if(result["phone"].length < 10){
+        console.log("Phone:",result["phone"].padStart(10, '0'));
+    }
+    else{
+        console.log("Phone:",(result["phone"]).slice(0,10));
     }
 
-    const trimArray = resarray.map(element => {
-        return element.trim();
-    });
-});
+    if((result["age"]>=150)){
+        result["age"] = result["age"].toString().replace(result["age"],"NA");
+        console.log("Age:",result["age"]);
+    }
+    else{
+        console.log("Age:",result["age"]);
+    }
+
+    result["id"] = result["id"].slice(3).padStart(result["id"].length,'*').trim();
+    console.log("Id:",result["id"]);
+}
---
+++ Exersice 10/js/script.js
@@ -0,0 +1,29 @@
+const a={ Parent: 
+            { Student: 
+                { Name: "Virat", Age: 24, Rollno: 34, marks: [ { English : 85 }, { Science: 75 } ] } , 
+                MobileNos: [ "1234567890", "9087654321" ] 
+            } 
+        };
+const b = JSON.parse(JSON.stringify(a)); 
+b.Parent.Student.Name = "Ashna";
+b.Parent.Student.Age = 23;
+b.Parent.Student.Rollno = 18;
+b.Parent.Student.marks[0].English = 95;
+b.Parent.Student.marks[1].Science = 70;
+b.Parent.MobileNos[0] = "9188508445";
+b.Parent.MobileNos[1] = "9188728202";
+
+console.log("Name a: ",a.Parent.Student.Name);
+console.log("Age a: ",a.Parent.Student.Age);
+console.log("Rollno a: ",a.Parent.Student.Rollno);
+console.log("Marks a: ",a.Parent.Student.marks[0].English);
+console.log("Marks a: ",a.Parent.Student.marks[1].Science);
+console.log("MobileNos a: ",a.Parent.MobileNos[0]);
+console.log("MobileNos a: ",a.Parent.MobileNos[1]);
+console.log("Name b: ",b.Parent.Student.Name);
+console.log("Age b: ",b.Parent.Student.Age);
+console.log("Rollno b: ",b.Parent.Student.Rollno);
+console.log("Marks b: ",b.Parent.Student.marks[0].English);
+console.log("Marks b: ",b.Parent.Student.marks[1].Science);
+console.log("MobileNos b: ",b.Parent.MobileNos[0]);
+console.log("MobileNos b: ",b.Parent.MobileNos[1]);
